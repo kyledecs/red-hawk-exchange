e5a3f319e811c521ef4da05149fbda2c75fe40cb
Mariana Silva <oliveirasilm1@montclair.edu>
2026-04-01 16:31:32 -0400

    Post Listing
    - user can actually submit listing and stores the form in the database listings table
    - user can upload photos with listing that is stored in bucket

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, session
 from supabase import create_client
 from dotenv import load_dotenv
 import os
+import uuid
 
 load_dotenv()
 
@@ -89,16 +90,55 @@ def login():
 def dashboard():
     if not session.get("logged_in"):
         return redirect("/login-page")
+    
+    response = supabase.table("listings").select("*").order("created_at", desc=True).execute()
+    listings = response.data
 
-    return render_template("dashboard.html", email=session.get("email"))
+    return render_template("dashboard.html", email=session.get("email"), listings=listings)
 
 # ------------ CREATE LISTING --------------
-@app.route("/createListing")
+@app.route("/createListing", methods=['GET','POST'])
 def createListing():
     if not session.get("logged_in"):
         return redirect("/login-page")
     
-    return render_template("createListing.html")
+    if request.method == 'POST':
+        
+        user = supabase.auth.get_user()
+        listerID = user.user.id
+        
+        uploadedFiles = request.files.getlist("photos")
+        photoURLS = []
+        
+        for file in uploadedFiles:
+            if file.filename != '':
+                fileExtension = file.filename.rsplit('.',1)[1]
+                uniqueName = f"{uuid.uuid4()}.{fileExtension}"
+                pathSupabase = f"{listerID}/{uniqueName}"
+                
+                fileData = file.read()
+                supabase.storage.from_("listingPhotos").upload(
+                    path=pathSupabase,
+                    file=fileData,
+                    file_options={'content-type': file.content_type})
+                
+                urlRes = supabase.storage.from_("listingPhotos").get_public_url(pathSupabase)
+                photoURLS.append(urlRes)
+
+        newListing = {
+            "lister_id": listerID,
+            "title": request.form.get("title"),
+            "price": request.form.get("price"),
+            "status": "Available",
+            "photos": photoURLS,
+            # EDIT HERE TO ADD MORE ATTRIBUTES TO POST LISTING
+        }
+        
+        response = supabase.table("listings").insert(newListing).execute()
+        
+        return redirect("/dashboard")
+    else:
+        return render_template("createListing.html")
 
 # ------------ PROFILE --------------
 @app.route("/profile")
